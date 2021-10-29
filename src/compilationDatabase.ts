edabcf7b51168a85b0e4f11a0c60844fc567347c
Paul Johnston <paul.johnston@robinhood.com>
2021-10-28 22:56:42 -0600

    Always show build results

--- src/compilationDatabase.ts
+++ src/compilationDatabase.ts
@@ -83,6 +83,7 @@ export function createBazelBuildAspectCommand(
         "--override_repository=bazel_vscode_compdb=" + repositoryPath,
         "--aspects=@bazel_vscode_compdb//:aspects.bzl%compilation_database_aspect",
         "--color=no",
+        "--show_result=2147483647", // MAX_INT
         "--noshow_progress",
         "--noshow_loading_progress",
         "--output_groups=compdb_files,header_files",
